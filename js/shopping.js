825e1c0da7042ae0ccf903f67b72c2c3fab56866
lysmex <jonna_jantunen@msn.com>
2023-10-02 16:44:40 +0300

    WS5 tekoa

--- js/shopping.js
+++ js/shopping.js
@@ -1,3 +1,147 @@
+// Tehtävä 1 / 4
+
+function tehtavaYksi(){
+
+  if (lomakeEmail == "") {
+    alert("Email is required!");
+    document.querySelector("#email").style.border = "3px solid red";
+    document.querySelector("#virhe").innerHTML =
+      "<strong>&nbsp;* Email is required</strong>";
+    return false;
+  }
+  if (lomakeEmail.length < 6) {
+    alert("Email is too short!");
+    document.querySelector("#email").style.border = "3px solid red";
+    document.querySelector("#virhe").innerHTML =
+      "<strong>&nbsp;* Email is too short</strong>";
+    return false;
+  }
+  if (lomakeEmail.length > 25) {
+    alert("Email is too long!");
+    document.querySelector("#email").style.border = "3px solid red";
+    document.querySelector("#virhe").innerHTML =
+      "<strong>&nbsp;* Email is too long</strong>";
+    return false;
+  }
+  if (!lomakeEmail.includes("@")) {
+    alert("Your emai address is missing a @!");
+    document.querySelector("#email").style.border = "3px solid red";
+    document.querySelector("#virhe").innerHTML =
+      "<strong>&nbsp;* Email is missing @</strong>";
+    return false;
+  }
+  if (lomakeComment == "") {
+    alert("Comment is required!");
+    document.querySelector("#comment").style.border = "3px solid red";
+    document.querySelector("#virhe2").innerHTML =
+      "<strong>* Comment is required</strong>";
+    return false;
+  }
+  if (lomakeComment.length > 50) {
+    alert("Comment needs to be less than 50 characters!");
+    document.querySelector("#comment").style.border = "3px solid red";
+    document.querySelector("#virhe2").innerHTML =
+      "<strong>&nbsp;* Comment is too long</strong>";
+    return false;
+  } else {
+    alert(lomakeEmail + ", " + lomakeComment);
+    return true;
+  }
+}
+
+let checkIfWantEmail = document.querySelector('#check_email');
+let checkIfWantCall = document.querySelector('#check_call');
+let checkIfWantSms = document.querySelector('#check_sms');
+
+let findEmailInput = document.querySelector('#give_email');
+let findPhoneCall = document.querySelector('#give_call');
+let findPhoneSms = document.querySelector('#give_sms');
+
+if (!checkIfWantEmail.checked){
+    console.log("ei email")
+        findEmailInput.style.display="none";
+} if (checkIfWantEmail.checked) { 
+    console.log("joo email")
+    findEmailInput.style.display="block";
+}
+
+if (!checkIfWantCall.checked){
+        findPhoneCall.style.display="none";
+} if (checkIfWantCall.checked) {
+    findPhoneCall.style.display="block";
+}
+
+if (!checkIfWantSms.checked){
+        findPhoneSms.style.display="none";
+} if(checkIfWantSms.checked) {
+    findPhoneSms.style.display="block";}
+
+// Tehtävä 2
+
+let paketti = document.querySelector("#type");
+let vuodet = document.querySelector("#years");
+let hintaDisplay = document.querySelector("#cost");
+let hinta = 0;
+
+function laskeNappi() {
+  if (paketti.value == "basic") {
+    hinta = 10 * vuodet.value;
+    if (vuodet.value > 2) {
+      document.querySelector("#notice").innerHTML =
+        "<strong>Eligible for 20% discount!</strong>";
+      hinta = hinta * 0.8;
+    }
+    if (vuodet.value >= 5) {
+      document.querySelector("#notice2").innerHTML =
+        "Enjoy another special discount!";
+      hinta = hinta - 5;
+    }
+    hintaDisplay.value = hinta;
+  }
+  if (paketti.value == "premium") {
+    hinta = 15 * vuodet.value;
+    if (vuodet.value > 2) {
+      document.querySelector("#notice").innerHTML =
+        "<strong>Eligible for 20% discount!</strong>";
+      hinta = hinta * 0.8;
+    }
+    if (vuodet.value >= 5) {
+      document.querySelector("#notice2").innerHTML =
+        "Enjoy another special discount!";
+      hinta = hinta - 5;
+    }
+    hintaDisplay.value = hinta;
+  }
+  if (paketti.value == "gold") {
+    hinta = 20 * vuodet.value;
+    if (vuodet.value > 2) {
+      document.querySelector("#notice").innerHTML =
+        "<strong>Eligible for 20% discount!</strong>";
+      hinta = hinta * 0.8;
+    }
+    if (vuodet.value >= 5) {
+      document.querySelector("#notice2").innerHTML =
+        "Enjoy another special discount!";
+      hinta = hinta - 5;
+    }
+    hintaDisplay.value = hinta;
+  }
+  if (paketti.value == "platinum") {
+    hinta = 25 * vuodet.value;
+    if (vuodet.value > 2) {
+      document.querySelector("#notice").innerHTML =
+        "<strong>Eligible for 20% discount!</strong>";
+      hinta = hinta * 0.8;
+    }
+    if (vuodet.value >= 5) {
+      document.querySelector("#notice2").innerHTML =
+        "Enjoy another special discount!";
+      hinta = hinta - 5;
+    }
+    hintaDisplay.value = hinta;
+  }
+}
+
 // shopping.js
 // This script calculates an order total.
 
@@ -6,72 +150,76 @@
 
 // Exercise: 3
 function calculate() {
-    'use strict';
+  "use strict";
 
-    // For storing the order total:
-    var total;
+  // For storing the order total:
+  var total;
 
-    // Get references to the form values:
-    var quantity = document.getElementById('quantity').value ; // add value
+  // Get references to the form values:
+  var quantity = document.getElementById("quantity").value; // add value
+  quantity = parseInt(quantity, 10);
 
-    var price =  document.getElementById('price').value ;
-    var tax =  document.getElementById('tax').value ; // add value
-    var discount =  document.getElementById('discount').value ; //add value
-	var shipping = document.getElementById('shipping').value; // add shipping
+  var price = document.getElementById("price").value;
+  price = parseFloat(price);
+  var tax = document.getElementById("tax").value; // add value
+  tax = parseFloat(tax);
+  var discount = document.getElementById("discount").value; //add value
+  discount = parseFloat(discount);
+  var shipping = document.getElementById("shipping").value; // add shipping
+  shipping = parseFloat(shipping);
 
-    // Add validation here later!
+  // Add validation here later!
 
-    // Calculate the initial total:
-    total = quantity * price;
-    console.log("total before tax: " + total);
+  // Calculate the initial total:
+  total = quantity * price;
+  console.log("total before tax: " + total);
 
-    // Make the tax rate easier to use:
-    tax = tax / 100;
-    tax = tax + 1;
+  // Make the tax rate easier to use:
+  tax = tax / 100;
+  tax = tax + 1;
 
-    // Factor in the tax:
-    total = total * tax;
-    console.log("total after tax: " + total);
+  // Factor in the tax:
+  total = total * tax;
+  console.log("total after tax: " + total);
 
-    // Factor in the discount:
-	if (quantity > 100){
-		total = total - (2*discount);
-	} else {
-		
-		total = total - discount;
-	}
-	total = total + (1.0*shipping);
-    console.log("total after discount: " + total);
+  // Factor in the discount:
+  if (quantity > 100) {
+    total = total - (2 * discount);
+  } else {
+    total = total - discount;
+  }
+  total = total + 1.0 * shipping;
+  console.log("total after discount: " + total);
 
-    // Format the total to two decimal places:
-    total = total.toFixed(2);
+  // Format the total to two decimal places:
+  total = total.toFixed(2);
 
-    // Display the total:
-    document.getElementById('total').value = total;
+  // Display the total:
+  document.getElementById("total").value = total;
 
-    // Return false to prevent submission:
-    return false;
-
+  // Return false to prevent submission:
+  return false;
 } // End of calculate() function.
+
 
 // Function called when the window has been loaded.
 // Function needs to add an event listener to the form.
 // Exercise: 2 using eventListener
-function init() {
-    'use strict';
+//function init() {
+//    'use strict';
 
-    // Add an event listener to the form:
-   
-    var theForm = document.getElementById('theForm');
-    theForm.addEventListener("submit",function(e){
-        if(!calculateMembership()){
-            // Prevent to upload the page 
-            e.preventDefault();
-        }
-    });
-}
+// Add an event listener to the form:
+
+//    var theForm = document.getElementById('theForm');
+//    theForm.addEventListener("submit",function(e){
+//        if(!calculateMembership()){
+//            // Prevent to upload the page
+//            e.preventDefault();
+//        }
+//    });
+//}
 
 // End of init() function.
 
 // Assign an event listener to the window's load event:
-window.onload = init;
+//window.onload = init;
