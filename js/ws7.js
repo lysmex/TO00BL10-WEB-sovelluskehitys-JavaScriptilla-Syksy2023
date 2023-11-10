6e4fae36cf114319c6a7c1f3654dde0035127f2e
lysmex <jonna_jantunen@msn.com>
2023-11-10 11:16:41 +0200

    ws7

--- js/ws7.js
+++ js/ws7.js
@@ -1,55 +1,76 @@
+// Tehtävä 1
+// luodaan funktio parseData ()
 function parseData() {
+  // tehdään muuttuja, joka hakee tiedostosta kaikki quote tagin sisällöt
+  var quoteXMLAr = document.querySelectorAll("quote");
+  // tehdään muuttuja, joka hakee tiedostosta kaikki author tagin sisällöt
+  var authorXMLAr = document.querySelectorAll("author");
 
-    var haeLainaus = XMLDocument.querySelectorAll("quote");
+  // tehdään muuttujat yksittäiselle quotelle ja authorille
+  var quoteXML;
+  var authorXML;
+
+  // luodaan looppi, jolla käydään läpi kaikki tiedostosta löytyvät quote ja author tagit
+  for (let i = 0; i < quoteXMLAr.length; i++) {
+
+    //tallennetaan yksitellen kaikkien quote ja author tagien sisältö muuttujaan
+    quoteXML = quoteXMLAr[i].innerHTML + "<br>";
+    authorXML = authorXMLAr[i].innerHTML + "<br><br>";
+    // loin html-tiedostoon uuden divin id:llä authorquote, jonka innerHTMLää muutetaan näyttämään äskeiset muuttujat
+    document.querySelector("#authorquote").innerHTML += quoteXML + authorXML;
+  }
 }
 
-function loadXMLFile() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "http://iceberg-cycle.codio.io/5:%20Asynchronous%20JavaScript%20(AJAX)/famous-quotes.xml", true);
-    xmlhttp.send();
+// Tehtävä 2
 
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("quotes").innerHTML = xmlhttp.responseText;
-        }
-    }
-}
+//function loadXMLFile() {
+//   var xmlhttp = new XMLHttpRequest();
+//  xmlhttp.open("GET", "http://iceberg-cycle.codio.io/5:%20Asynchronous%20JavaScript%20(AJAX)/famous-quotes.xml", true);
+//xmlhttp.send();
 
-// tehtävä 4
+//    xmlhttp.onreadystatechange = function () {
+//      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//        document.getElementById("quotes").innerHTML = xmlhttp.responseText;
+//  }
+//}
+//}
+
+// Tehtävä 4
 // funktion parametri url kutsutaan html-tiedostossa onclickevent kohdassa
 function loadAndParseNews(url) {
-    //tehdään ajax kutsu (tämä on "aina sama")
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+  //tehdään ajax kutsu (tämä on "aina sama")
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 
-    // määritellään vastauksen käsittelijä (määrittely, virheiden tsekkaus ja datan määrittely on "aina sama")
-    xmlhttp.onreadystatechange = function () {
-        // tarkistetaan virheet
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            data = xmlhttp.responseXML;
-            // valitaan xml datasta kaikki tägit "title"
-            let uutiset = data.querySelectorAll("title");
-            // valitaan xml datasta kaikki tägit "link"
-            let linkit = data.querySelectorAll("link");
-            let uutinen;
-            let linkki;
+  // määritellään vastauksen käsittelijä (määrittely, virheiden tsekkaus ja datan määrittely on "aina sama")
+  xmlhttp.onreadystatechange = function () {
+    // tarkistetaan virheet
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      data = xmlhttp.responseXML;
+      // valitaan xml datasta kaikki tägit "title"
+      let uutiset = data.querySelectorAll("title");
+      // valitaan xml datasta kaikki tägit "link"
+      let linkit = data.querySelectorAll("link");
+      let uutinen;
+      let linkki;
 
-            // käydään for-loopilla läpi kaikki xml datasta löytyvät tägit "title" (koska muuttuja uutiset hakee titlet ja uutinen hakee yksitellen uutiset muuttujan innerHTML:n)
-            for (let i = 0; i < uutiset.length; i++) {
-                // uutinen hakee yksitellen muuttujan uutiset innerHTML:n
-                uutinen = uutiset[i].innerHTML + "<br>";
-                // tulostetaan uutiset konsoliin
-                console.log(uutinen);
-                linkki = linkit[i].innerHTML;
-                // haetaan html tiedostosta div elementti, joka id on newsfeed ja muutetaan sen innerHTML:ksi lista, joka sisältää linkin (joka haettu xml datasta tägillä link) ja uutisen (joka haettu xml datasta tägillä title)
-                // += tekee sen, että uusi haettu uutinen lisätään aina viimeisimmän perään. pelkkä = merkki tekisin sen, että uutisen haku aina korvaisi edellisen linen, jolloin diviin tulostuisi vain viimeisin uutinen
-                document.querySelector("#newsfeed").innerHTML += "<li><a href='" + linkki + "' target='_blank'>" + uutinen + "</a>";
+      // käydään for-loopilla läpi kaikki xml datasta löytyvät tägit "title" (koska muuttuja uutiset hakee titlet ja uutinen hakee yksitellen uutiset muuttujan innerHTML:n)
+      for (let i = 0; i < uutiset.length; i++) {
+        // uutinen hakee yksitellen muuttujan uutiset innerHTML:n
+        uutinen = uutiset[i].innerHTML + "<br>";
+        // tulostetaan uutiset konsoliin
+        console.log(uutinen);
+        linkki = linkit[i].innerHTML;
+        // haetaan html tiedostosta div elementti, joka id on newsfeed ja muutetaan sen innerHTML:ksi lista, joka sisältää linkin (joka haettu xml datasta tägillä link) ja uutisen (joka haettu xml datasta tägillä title)
+        // += tekee sen, että uusi haettu uutinen lisätään aina viimeisimmän perään. pelkkä = merkki tekisin sen, että uutisen haku aina korvaisi edellisen linen, jolloin diviin tulostuisi vain viimeisin uutinen
+        document.querySelector("#newsfeed").innerHTML +=
+          "<li><a href='" + linkki + "' target='_blank'>" + uutinen + "</a>";
 
-                // linkki voidaan myös rakentaa näin
-                // let rivi = `<a href="${linkit[i].innerHTML}">{$uutinen}</a><br>`
-                // document.querySelector("#newsfeed").innerHTML += rivi;
-            }
-        }
+        // linkki voidaan myös rakentaa näin
+        // let rivi = `<a href="${linkit[i].innerHTML}">{$uutinen}</a><br>`
+        // document.querySelector("#newsfeed").innerHTML += rivi;
+      }
     }
+  };
 }
